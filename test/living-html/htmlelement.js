c51bb80ea8f26d366302063378200edd46e0756c
Sebastian Mayr <sebmaster16@gmail.com>
2015-03-20 17:13:06 +0000

    Add tests for HTMLUnknownElement prototype chain

---
+++ test/living-html/htmlelement.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+var jsdom = require("../..");
+
+exports["unknown elements should return HTMLUnknownElement"] = function (t) {
+  t.expect(2);
+
+  var doc = jsdom.jsdom();
+
+  var el = doc.createElement("foobar");
+  t.ok(el instanceof doc.defaultView.HTMLUnknownElement, "unknown element should inherit from HTMLUnknownElement");
+  t.ok(el instanceof doc.defaultView.HTMLElement, "unknown element should inherit from HTMLElement too");
+
+  t.done();
+};
+
+exports["other elements should have their respective types"] = function (t) {
+  t.expect(2);
+
+  var doc = jsdom.jsdom();
+
+  var el = doc.createElement("div");
+  t.ok(el instanceof doc.defaultView.HTMLDivElement, "div element should inherit from HTMLDivElement");
+  t.ok(el instanceof doc.defaultView.HTMLElement, "div element should inherit from HTMLElement too");
+
+  t.done();
+};
